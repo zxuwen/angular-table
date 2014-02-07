cf6297edf03bc3353deb97758214876eebe95a76
Samuel Mueller <mueller.samu@gmail.com>
2014-02-07 20:25:01 +0100

    updated karma configuration for newest version

--- karma.conf.js
+++ karma.conf.js
@@ -1,82 +1,81 @@
 // Karma configuration
-// Generated on Thu Jul 04 2013 23:17:10 GMT+0200 (CEST)
+// Generated on Wed Feb 05 2014 13:15:20 GMT+0100 (CET)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  "http://code.jquery.com/jquery-1.10.1.min.js",
-  "http://code.angularjs.org/1.1.5/angular.min.js",
-  "http://code.angularjs.org/1.1.5/angular-mocks.js",
-  "http://underscorejs.org/underscore-min.js",
-  // important to load this first, because it contains the module definition
-  "coffee/atTable.coffee",
-  "coffee/*.coffee",
-  "test/test_helper.coffee",
-  "test/*.coffee",
-  "test/templates/*.html",
-  "test/templates/*/*.html"
-];
+    // list of files / patterns to load in the browser
+    files: [
+      "http://code.jquery.com/jquery-1.10.1.min.js",
+      "http://code.angularjs.org/1.2.11/angular.min.js",
+      "http://code.angularjs.org/1.2.11/angular-mocks.js",
+      "http://underscorejs.org/underscore-min.js",
+      'coffee/atTable.coffee',
+      'coffee/*.coffee',
+      'test/test_helper.coffee',
+      'test/*.coffee',
+      'test/templates/**/*.html'
+    ],
 
-preprocessors = {
-  "coffee/*.coffee": "coffee",
-  "test/*.coffee": "coffee",
-  "test/templates/*.html": "html2js",
-  "test/templates/*/*.html": "html2js"
+
+    // list of files to exclude
+    exclude: [
+
+    ],
+
+    preprocessors: {
+      "coffee/*.coffee": ["coffee"],
+      "test/*.coffee": ["coffee"],
+      "test/templates/**/*.html": ["ng-html2js"]
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
 };
-
-// list of files to exclude
-exclude = [
-
-];
-
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
