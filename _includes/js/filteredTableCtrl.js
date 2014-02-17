cfe8d02e72c5fca7b5bb65befa32ce6eabd7d946
Samuel Mueller <mueller.samu@gmail.com>
2014-02-17 22:36:18 +0100

    added delete functionality

--- _includes/js/filteredTableCtrl.js
+++ _includes/js/filteredTableCtrl.js
@@ -1,11 +1,21 @@
 angular.module("angular-table-example").controller("filteredTableCtrl", ["$scope", "$filter", function($scope, $filter) {
 
-  $scope.list = $scope.$parent.personList
+  $scope.list = $scope.$parent.personList;
 
-  $scope.filteredList = $scope.list
+  $scope.filteredList = $scope.list;
 
-  $scope.queryChanged = function(query) {
-    $scope.filteredList = $filter("filter")($scope.list, query);
+  // $scope.getList = function() {
+  //   return $filter("filter")($scope.list, $scope.query);
+  // }
+
+  $scope.del = function(i) {
+    console.log("index: " + i);
+    $scope.list.splice(i, 1);
+    $scope.updateFilteredList();
+  }
+
+  $scope.updateFilteredList = function() {
+    $scope.filteredList = $filter("filter")($scope.list, $scope.query);
   };
 
 }])
