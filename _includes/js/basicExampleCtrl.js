f413f98e48a9b5f9cc57c24e891375e6376f5c36
Samuel Mueller <mueller.samu@gmail.com>
2014-02-27 20:30:49 +0100

    imported newest version and updated examples

--- _includes/js/basicExampleCtrl.js
+++ _includes/js/basicExampleCtrl.js
@@ -1,3 +1,7 @@
 angular.module("angular-table-example").controller("basicExampleCtrl", ["$scope", function($scope) {
   $scope.list = $scope.$parent.personList
-}])
+  $scope.config = {
+    itemsPerPage: 5,
+    fillLastPage: true
+  }
+}]);
